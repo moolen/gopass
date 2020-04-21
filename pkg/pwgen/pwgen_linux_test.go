44fffcb43a28721b7dd27b48f40bf9f5ac064c8f
Moritz Johner <beller.moritz@googlemail.com>
2020-04-21 08:32:27 +0200

    fix: unset env after test

--- pkg/pwgen/pwgen_linux_test.go
+++ pkg/pwgen/pwgen_linux_test.go
@@ -9,5 +9,6 @@ import (
 
 func TestPwgenExternal(t *testing.T) {
 	_ = os.Setenv("GOPASS_EXTERNAL_PWGEN", "echo foobar")
+	defer os.Unsetenv("GOPASS_EXTERNAL_PWGEN")
 	assert.Equal(t, "foobar", GeneratePassword(4, true))
 }
